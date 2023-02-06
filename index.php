9020fcfc4d6039f2c0e697f70da2fd47f062cf3c
MyUsername <my@email.com>
2023-02-06 22:07:08 +0100

    update exercise

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/models/Product.php';
 require_once __DIR__ . '/models/Category.php';
 require_once __DIR__ . '/models/Food.php';
 require_once __DIR__ . '/models/Kennel.php';
+require_once __DIR__ . '/db/db.php';
 
 ?>
 
@@ -14,37 +15,43 @@ require_once __DIR__ . '/models/Kennel.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php E-commerce</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 
 <body>
-    <?php
+    <main class="">
+        <ul class="flex flex-wrap"> <?php foreach ($productList as $item) { ?>
+                <li class="w-full md:w-1/2 p-4">
 
-    $productList = [
-        $eridania = new Kennel(new Category('cani'), 'Eridania', 1.50, 'stoffa', '1.3m'),
-        $croccantini = new Food(new Category('gatti'), 'Croccanti', 5.50, 'pepe', '01/03/2023'),
-        $bocconcini = new Food(new Category('gatti'), 'Bocconcini Boccono', 20.45, 'carne', '01/03/2030'),
-        $bama = new Kennel(new Category('cani'), 'Bama Super 2', 50.50, 'cashmire', '5m'),
-        $doggy = new Kennel(new Category('cani'), 'Doggy 2', 30.99, 'flanella', '1.5m'),
-    ];
-
-    foreach ($productList as $item) {
-        echo '<article>';
-        echo "<img src='{$item->getGenre()->getImgPath()}' alt=''>";
-        echo '<h1>' . $item->getName() . '</h1>';
-        echo '<p>' . $item->getPrice() . '</p>';
-        if ($item instanceof Food) {
-            echo '<p>' . $item->getIngredients() . '</p>';
-            echo '<p>' . $item->getExpireDate() . '</p>';
-        }
-        if ($item instanceof Kennel) {
-            echo '<p>' . $item->getMaterial() . '</p>';
-            echo '<p>' . $item->getSize() . '</p>';
-        }
-        echo '<hr>';
-        echo '</article>';
-    }
-
-    ?>
+                    <div class="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b rounded-l  lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                        <div class="">
+                            <div>
+                                <div class="title flex items-start">
+                                    <div class="text-gray-900 font-bold text-xl mb-2 mr-5"><?php echo $item->getName() ?></div>
+                                    <img class="w-6 h-6 rounded-full mr-1" src="<?php echo $item->getGenre()->getImgPath() ?>" alt="<?php echo $item->getName() ?>">
+                                </div>
+                            </div>
+                            <div class="flex justify-between">
+                                <div class="info">
+                                    <p class="text-gray-500 text-base"><span class="text-black font-medium">Price </span> <?php echo $item->getPrice() ?></p>
+                                    <?php if ($item instanceof Food) { ?>
+                                        <p class="text-gray-500 text-base"><span class="text-black font-medium">Ingredients: </span> <?php echo $item->getIngredients() ?></p>
+                                        <p class="text-gray-500 text-base"><span class="text-black font-medium">Exp. date: </span><?php echo $item->getExpireDate() ?></p>
+                                    <?php } ?>
+                                    <?php if ($item instanceof Kennel) { ?>
+                                        <p class="text-gray-500 text-base"><span class="text-black font-medium">Ingredients: </span> <?php echo $item->getMaterial() ?></p>
+                                        <p class="text-gray-500 text-base"><span class="text-black font-medium">Exp. date: </span><?php echo $item->getSize() ?></p>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </li>
+            <?php } ?>
+        </ul>
+    </main>
 </body>
 
 </html>
---
+++ models/Cart.php
@@ -0,0 +1,45 @@
+<?php
+require_once __DIR__ . '/Product.php';
+
+class Cart
+{
+    protected $totalPrice;
+    protected $quantity;
+
+    public function __construct($_initialPrice, $_initialQuantity)
+    {
+        $this->setInitialPrice($_initialPrice);
+        $this->setInitialQuantity($_initialQuantity);
+    }
+
+    public function getTotalPrice()
+    {
+        return $this->totalPrice;
+    }
+
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+
+    public function setInitialPrice($_initialPrice)
+    {
+        $this->totalPrice = $_initialPrice;
+    }
+
+    public function setInitialQuantity($initialQuantity)
+    {
+        $this->quantity = $initialQuantity;
+    }
+
+    public function addNewProduct()
+    {
+        $this->quantity += 1;
+    }
+
+    public function addNewPrice($priceToAdd)
+    {
+        $this->totalPrice += $priceToAdd;
+    }
+}
